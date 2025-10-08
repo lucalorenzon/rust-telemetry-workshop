e2808a68e1f6112a78f80ddd60b0f002d1ac85fa
Luca Lorenzon <1104352+lucalorenzon@users.noreply.github.com>
2025-10-08 07:48:59 +0200

    feat: done 01_structured_logging/06_instrument_macro

--- exercises/01_structured_logging/06_instrument_macro/src/lib.rs
+++ exercises/01_structured_logging/06_instrument_macro/src/lib.rs
@@ -9,14 +9,20 @@
 mod subscriber;
 
 pub use subscriber::init_test_subscriber;
+use tracing::instrument;
 
 /// Given a list of order numbers, compute the total price.
+#[instrument(name = "process total price", skip(order_numbers), fields(outcome))]
 pub fn get_total(order_numbers: &[u64]) -> Result<u64, anyhow::Error> {
     let mut total = 0;
     for order_number in order_numbers {
-        let order_details = get_order_details(*order_number)?;
+        let order_details = get_order_details(*order_number).map_err(|err| {
+            tracing::Span::current().record("outcome", "failure");
+            err
+        })?;
         total += order_details.price;
     }
+    tracing::Span::current().record("outcome", "success");
     Ok(total)
 }
 
@@ -26,11 +32,14 @@ pub struct OrderDetails {
 }
 
 /// A dummy function to simulate what would normally be a database query.
+#[instrument(name = "retrieve order", skip(order_number), fields(outcome))]
 fn get_order_details(order_number: u64) -> Result<OrderDetails, anyhow::Error> {
     if order_number % 4 == 0 {
+        tracing::Span::current().record("outcome", "failure");
         Err(anyhow::anyhow!("Failed to talk to the database"))
     } else {
         let prices = vec![999, 1089, 1029];
+        tracing::Span::current().record("outcome", "success");
         Ok(OrderDetails {
             order_number,
             price: prices[order_number as usize % prices.len()],
