84cc614b0ab7e5811fe20a519d7ca21e4d1e7479
Luca Lorenzon <1104352+lucalorenzon@users.noreply.github.com>
2025-10-08 07:01:02 +0200

    feat: done 01_structured_logging/04_tracing

--- exercises/01_structured_logging/04_tracing/src/lib.rs
+++ exercises/01_structured_logging/04_tracing/src/lib.rs
@@ -1,11 +1,12 @@
 //! # Exercise
 //!
-//! We'll ignore the consumer side for now and focus on the instrumentation side.  
+//! We'll ignore the consumer side for now and focus on the instrumentation side.
 //! We'll redo the previous exercise using the `tracing` crate in order to get familiar with the
 //! basics.
 mod subscriber;
 
 pub use subscriber::init_test_subscriber;
+use tracing::info_span;
 
 /// Given a list of order numbers, compute the total price.
 ///
@@ -18,7 +19,15 @@ pub use subscriber::init_test_subscriber;
 pub fn get_total(order_numbers: &[u64]) -> Result<u64, anyhow::Error> {
     // Tip: use `tracing::info_span!` to create a new span.
     // You'll have to learn about the *RAII guard* pattern!
-    todo!()
+    let span = info_span!("process total price");
+    span.in_scope(|| {
+        let mut sum = 0;
+        for oid in order_numbers {
+            let order = get_order_details(*oid)?;
+            sum += order.price;
+        }
+        Ok(sum)
+    })
 }
 
 pub struct OrderDetails {
@@ -28,13 +37,15 @@ pub struct OrderDetails {
 
 /// A dummy function to simulate what would normally be a database query.
 fn get_order_details(order_number: u64) -> Result<OrderDetails, anyhow::Error> {
-    if order_number % 4 == 0 {
-        Err(anyhow::anyhow!("Failed to talk to the database"))
-    } else {
-        let prices = vec![999, 1089, 1029];
-        Ok(OrderDetails {
-            order_number,
-            price: prices[order_number as usize % prices.len()],
-        })
-    }
+    info_span!("retrieve order").in_scope(|| {
+        if order_number % 4 == 0 {
+            Err(anyhow::anyhow!("Failed to talk to the database"))
+        } else {
+            let prices = vec![999, 1089, 1029];
+            Ok(OrderDetails {
+                order_number,
+                price: prices[order_number as usize % prices.len()],
+            })
+        }
+    })
 }
